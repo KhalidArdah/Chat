330e8e297baca664ba352bf0fc6ecad8333185fe
KhalidArdah <khalid.jamal.4002@gmail.com>
2016-08-20 00:44:39 +0300

    ok

---
+++ app/src/main/java/com/khalid/gazzawi/chat/Conversation.java
@@ -0,0 +1,19 @@
+package com.khalid.gazzawi.chat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Khalid on 8/20/16.
+ */
+public class Conversation extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.conversation,container,false);
+    }
+}
